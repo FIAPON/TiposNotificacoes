01522849f6e09a0b5d4df41c1d3325c38c2ec30d
Heider Lopes <heidertreinamentos@gmail.com>
2020-09-12 16:14:22 -0300

    Initial commit

---
+++ app/src/main/java/br/com/heiderlopes/notification/DetailActivity.kt
@@ -0,0 +1,19 @@
+package br.com.heiderlopes.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        txtMessage.text = intent.getStringExtra(EXTRA_MESSAGE)
+    }
+
+    companion object {
+        const val EXTRA_MESSAGE = "message"
+    }
+}
